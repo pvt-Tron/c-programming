cc6822858851b02b1a934315245d3ae24210b183
pvt-Tron <pv.tassi@gmail.com>
2023-04-05 05:30:24 +0200

    stage_and_commit

---
+++ C Programming/Module 1/Limitations.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    double a=22.123456789123456;
+    
+    printf("%0.15lf",a);
+    return 0;
+}
+   // float a
+   //double a
+   // int a=1000000000;
+   // long long int b=1000000000;
+   //scanf("%da %db",&a,&b);
+   //printf("%da %db",a,b);
---
+++ C Programming/Module 1/output1st.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int rahim,karim;
+    char c;
+    float f;
+    scanf("%d %f %c",&rahim,&f,&c);
+    printf("%d %0.2f %c",rahim,f,c);
+   return 0;
+    // scanf("%d %f %c",&rahim,&f,&c);
+   // printf("%d %f %c",rahim,karim,habib);
+}
